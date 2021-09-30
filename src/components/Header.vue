1c23e2b77035ae9c3b07abb44d0e4d48928519d0
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2021-09-30 15:08:41 +0500

    10-dars

---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+  <!-- header -->
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img class="navbar__brand" src="/logo/frulogo.svg" alt />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li v-for="(link, index) in links" :key="index" class="nav-item">
+              <a
+                class="nav-link"
+                :class="{'active': index == activeIndex}"
+                aria-current="page"
+                :href="link.link"
+              >{{ link.title }}</a>
+            </li>
+            <li class="nav-item dropdown">
+              <b-dropdown size="lg" variant="link" toggle-class="text-decoration-none" no-caret>
+                <template #button-content>
+                  <div
+                    style="width: 33px; height: 33px; padding: 7px; border-radius: 100%; border: 1px solid #93c92b; border-radius: 50%; line-height: 0.8"
+                  >
+                    <img src="/logo/menu.svg" />
+                  </div>
+                </template>
+                <b-dropdown-item href="#">Action</b-dropdown-item>
+                <b-dropdown-item href="#">Another action</b-dropdown-item>
+                <b-dropdown-item href="#">Something else here...</b-dropdown-item>
+              </b-dropdown>
+            </li>
+          </ul>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown2">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link ps-0" href="#">
+                <div class="social-link">
+                  <img src="/logo/instagram.svg" />
+                </div>
+              </a>
+              <a class="nav-link ps-0" href="#">
+                <div class="social-link">
+                  <img src="/logo/facebook.svg" />
+                </div>
+              </a>
+              <a class="nav-link ps-0" href="#">
+                <div class="social-link">
+                  <img src="/logo/youtube.svg" />
+                </div>
+              </a>
+            </li>
+            <li class="nav-item" id="navflex">
+              <a class="nav-link pb-0" id="fome" href="#">Связаться с нами</a>
+              <a class="nav-link pt-0" href="#">+998 90 995 5959</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <!-- header end -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { link: "#", title: "Главная" },
+        { link: "#", title: "О компании" },
+        { link: "#", title: "Продукции" },
+        { link: "#", title: "Контакты" }
+      ],
+      activeIndex: 3
+    };
+  }
+};
+</script>
+
+<style>
+.nav-item .active {
+  color: red !important;
+}
+</style>
---
+++ src/components/ProductSlider.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="slide">
+    <div class="container">
+      <div class="slide__content row">
+        <div class="slide__title col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+          Продукции
+          <span class="ontheground">Frutata</span>
+        </div>
+        <a href="#" class="slide__more col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">Все продукции</a>
+      </div>
+    </div>
+    <div class="image-slider swiper-container" style="padding: 10px 10px;">
+      <div class="image-slider__wrapper swiper-wrapper">
+        <carousel items="5.5">
+          <div
+            class="image-slider__slide swiper-slide"
+            style="margin-right: 20px"
+            v-for="(item, index) in items"
+            :key="index"
+          >
+            <div id="bp" class="image-slider__image card">
+              <div class="kimlip">
+                <img class="card__img" :src="item.photo" alt />
+              </div>
+              <div class="card__cont">
+                <div class="card__title">{{ item.title }}</div>
+                <div class="card__txt">{{ item.text }}</div>
+                <div class="card__more">Подробнее</div>
+              </div>
+            </div>
+          </div>
+        </carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+  components: { carousel },
+  data() {
+    return {
+      items: [
+        {
+          photo: "/img/2.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        },
+        {
+          photo: "/img/3.png",
+          title: "В пакетная упаковка",
+          text: "Микс «Персик, груша, инжир»"
+        },
+        {
+          photo: "/img/4.png",
+          title: "В пакетная упаковка",
+          text: "Грушевые чипсы"
+        },
+        {
+          photo: "/img/2.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        },
+        {
+          photo: "/img/3.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        },
+        {
+          photo: "/img/4.png",
+          title: "В пакетная упаковка",
+          text: "Грушевые чипсы"
+        },
+        {
+          photo: "/img/2.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        },
+        {
+          photo: "/img/3.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        },
+        {
+          photo: "/img/4.png",
+          title: "В пакетная упаковка",
+          text: "Грушевые чипсы"
+        },
+        {
+          photo: "/img/2.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        },
+        {
+          photo: "/img/3.png",
+          title: "В пакетная упаковка",
+          text: "Фруктовый микс «Слива, яблоко, абрикос»"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
